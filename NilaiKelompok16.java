e53e08f1d9f83ba8bcfcbb1b1c9f6622cc47130a
dancelhal2000 <aobinaobin15@gmail.com>
2025-10-30 09:17:20 +0700

    add

---
+++ NilaiKelompok16.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NilaiKelompok16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nilai;
+        float totalNilai;
+        float rataRata;
+
+        int i = 1;
+        while (i <= 6) {
+            System.out.println("kelompok " + i);
+
+            totalNilai = 0;
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("nilai dari kelompok penilai " + j + " : ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+            rataRata = totalNilai / 5;
+            System.out.println("rata-rata nilai kelompok " + i + " : " + rataRata);
+            i++;
+        }
+
+    }
+}
